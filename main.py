77b3541dc7abe3d2f8fff54bbf1faee600051c56
andrea-bradbury <79626254+andrea-bradbury@users.noreply.github.com>
2021-08-23 16:18:22 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Python Guessing Game
+
+Task: Pick a number in a certain range. Make sure the player knows what that range is.
+The user guesses the number. If they guess right, they win. Otherwise, have them try again.
+After a number of guess, the player loses. Alternatively, implement a simple scoring algorithm. Because of the simplicity of this game, you could implement a hint.
+
+
+
+'''
